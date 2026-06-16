133d4b0d9ed54c50ebfd6e9175629515cae6ed73
bugcipher <554ravina@gmail.com>
2026-06-16 11:45:15 +0530

    Configure API URL environment variables for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create Axios Instance with default settings
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   headers: {
     'Content-Type': 'application/json'
   }
