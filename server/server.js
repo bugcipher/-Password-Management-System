ecd3b0aab1dacbac4404288b366af577b787551f
bugcipher <554ravina@gmail.com>
2026-06-16 12:00:08 +0530

    Allow all CORS origins for deployment

--- server/server.js
+++ server/server.js
@@ -10,10 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: 'http://localhost:5173', // Vite development port
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Console logs for incoming requests
